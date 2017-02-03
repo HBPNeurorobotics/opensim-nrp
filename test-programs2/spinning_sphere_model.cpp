81ef4b217e2b8a652b903bc881e2714dd936eb8f
Michael Welter <welter@truphysics.com>
2017-02-03 18:34:42 +0100

    Actually, my assumption that OpenSim::Model.buildSystem keeps the multibody system reference valid was wrong ...

--- test-programs2/spinning_sphere_model.cpp
+++ test-programs2/spinning_sphere_model.cpp
@@ -5,6 +5,16 @@
 using SimTK::Vec3;
 using namespace std;
 
+#define MODEL_OWNS_BODIES 0
+
+std::unique_ptr<SimTK::Integrator> IntegratorFactory(const SimTK::MultibodySystem &system)
+{
+  // Integrator uses pimpl ideom, but has no move nor assignment operator, so we are better off handling it through a pointer.
+  auto integ = std::make_unique<SimTK::RungeKuttaMersonIntegrator>(system);
+  integ->setAccuracy(1.0e-6);
+  return integ;
+}
+
 
 void testSpinningSphere() // actually it is just sitting there at the moment
 {
@@ -15,8 +25,15 @@ void testSpinningSphere() // actually it is just sitting there at the moment
   
         // Setup OpenSim model
         OpenSim::Model osimModel;
+        // We have to call buildSystem in order to obtain the multibody system for the integrator.
+        osimModel.buildSystem();
+        auto integrator = IntegratorFactory(osimModel.getMultibodySystem());
+        
+#if (!MODEL_OWNS_BODIES)
+        osimModel.updBodySet().setMemoryOwner(false);
+#endif
         OpenSim::Body& osimGround = osimModel.getGroundBody();
-        osimModel.setGravity(Vec3(0));
+        osimModel.setGravity(Vec3(0));        
         
         auto osimSphere = std::make_unique<OpenSim::Body>("sphere", mass, Vec3(0), mass * SimTK::Inertia::sphere(radius));
 
@@ -39,18 +56,23 @@ void testSpinningSphere() // actually it is just sitting there at the moment
         jointCoordinateSet[4].setDefaultValue(start_height); // set y-translation which is height
         jointCoordinateSet[0].setDefaultSpeedValue(3.14 * 2.);  // 2 pi per sec ?!
         
-        // Model takes ownership!        
+        // Model takes ownership by default!        
         // However, we could use osimModel.updBodySet().setMemoryOwner(false);
         // to keep owenership of the body, and all bodies and possibly everything added to the model.
+#if MODEL_OWNS_BODIES
         osimModel.addBody(osimSphere.release());
+#else
+        osimModel.addBody(osimSphere.get());
+#endif
+        // Since the topology changed (new object added), we have to rebuild the internal data structures.
+        osimModel.buildSystem();
+        // Internal pointer to the MultibodySystem got invalidated since the old system got deleted. Thus we have to get a new integrator, as well.
+        integrator = IntegratorFactory(osimModel.getMultibodySystem());
+
+        // Having done that, we can initialize and obtain our state vector.
+        // Alternatively, initSystem can be called which invokes buildSystem and initializeState.
+        SimTK::State& osimState = osimModel.initializeState();
                 
-        // Initialize the system and get the default state
-        SimTK::State& osimState = osimModel.initSystem();
-        
-        // Must come after osimModel.initSystem, otherwise BOOM.
-        SimTK::RungeKuttaMersonIntegrator integrator(osimModel.getMultibodySystem());
-        integrator.setAccuracy(1.0e-6);
-        
         // Regarding the life time of the joint: Deletion of blockToGround 
         // at this point results in a segfault. Therefore, I think that 
         // OpenSim::FreeJoint and friends must stay alive during the lifetime
@@ -61,7 +83,7 @@ void testSpinningSphere() // actually it is just sitting there at the moment
         std::cout << osimState.getQ() << std::endl;
         
         // Create the manager managing the forward integration and its outputs
-        OpenSim::Manager manager(osimModel,  integrator);
+        OpenSim::Manager manager(osimModel,  *integrator);
 
         // Print out details of the model
         //osimModel.printDetailedInfo(osimState, cout);
@@ -73,7 +95,11 @@ void testSpinningSphere() // actually it is just sitting there at the moment
         while (true)
         {
           std::cout << "t = " << t << " state = " << osimState.getQ() << std::endl;
-
+#if (!MODEL_OWNS_BODIES)
+          osimModel.getMultibodySystem().realize(osimState, SimTK::Stage::Velocity);
+          SimTK::Transform pose = osimModel.getSimbodyEngine().getTransform(osimState, *osimSphere);
+          std::cout << "sphere pose = " << pose << std::endl;
+#endif
           if (++i > Nsteps) break;
           
           // Does the right thing! Starts at t and steps forward until t+dt.
